1e087740ce786236d9bd660555e7aa5fecd3c84f
unknown <Don.71rus@mail.ru>
2020-01-24 22:25:16 +0300

    Finish

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Podchet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(27, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(428, 326);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(472, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(469, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Всего символов: ";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(469, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Без повтора:";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(472, 116);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 29);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(472, 55);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(84, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Без знаков";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button3.Location = new System.Drawing.Point(472, 224);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 37);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Сохранить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(428, 327);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.Visible = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(472, 78);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(120, 17);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "Вывести таблицей";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 367);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Анализатор текста:: С.Д.Н.";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Podchet
+{
+    public partial class Form1 : Form
+    {
+        string sav = "";
+        public Form1()
+        {
+            InitializeComponent();
+            button3.Visible = false;
+            ToolTip t = new ToolTip();
+            t.SetToolTip(checkBox1, "Ведется подсчет только букв и цифр. Исключая такие символы как: .,?!'@' \n \r");
+        }
+        public class table
+        {
+            public table(char ch,int i,double d)
+            {
+                simvol = ch;
+                count = i;
+                veroitnost = d;
+            }
+            public char simvol {get;set;}
+            public int count {get;set;}
+            public double veroitnost { get; set; }
+        }
+        public int sim_col(string s,char h)
+        {
+            int count = 0;
+            for (int i = 0; i < s.Count();i++ )
+            {
+                if(s[i]==h)
+                {
+                    count++;
+                }
+            }
+                return count;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<table> tb = new List<table> { };
+            var text = textBox1.Text;
+            if (checkBox1.Checked == true)
+            {
+                List<char> aa = new List<char>();
+                foreach (var i in text)
+                {
+                    aa.Add(i);
+                }
+
+                text = "";
+                aa.Remove('\r');
+                foreach (var i in aa)
+                {
+                    if (',' == i || '.' == i || '"' == i || '?' == i || '!' == i || '@' == i || '\n' == i)
+                    {
+                        text += ' ';
+                    }
+                    else { text += i; }
+
+                }
+            }
+            // text = aa.ToString();
+            textBox1.Text = text;
+            label1.Text = "Всего символов: " + text.Count();
+
+            var distinct_text = text.Distinct();
+            label2.Text = "Без повтора:" + distinct_text.Count();
+            foreach (var a in distinct_text)
+            {
+                tb.Add(new table(a, sim_col(text, a), (double)Math.Round((double)sim_col(text, a)/ text.Count(), 3)));
+            }
+
+            textBox1.Text += "\r\n";
+
+            textBox1.Text += "Символ" + "\t\n" + "Количество" + "\t" + "Вероятность" + "\r\n";
+            sav += "\"Символ\";"+ "\"Количество\";" + "\"Вероятность\";" + "\n";
+            foreach (var i in tb)
+            {
+
+                textBox1.Text += i.simvol + "\t\n    " + i.count + "\t                        " + Math.Round((double)i.count / text.Count(), 3) + "\r\n";
+                sav += "\"" + i.simvol + "\";\"" + i.count + "\";\"" + Math.Round((double)i.count / text.Count(), 3) + "\";" + "\n";
+            }
+            sav += "\n\"Всего символов\";" + "\"" + text.Count() + "\";";
+            sav += "\n\"Без повтора\";" + "\"" + distinct_text.Count() + "\";";
+            button3.Visible = true;
+            if (checkBox2.Checked == true)
+            {
+               // dataGridView1.AutoGenerateColumns = false;                
+                dataGridView1.DataSource =tb;                
+                dataGridView1.Visible = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button3.Visible = false;
+            sav = "";
+            dataGridView1.Visible = false;
+            dataGridView1.DataSource = null;
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                dataGridView1.Rows.Remove(dataGridView1.Rows[dataGridView1.Rows.Count - 1]);
+            textBox1.Clear();
+        }       
+        private void button3_Click(object sender, EventArgs e)
+        {          
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовый документ (*.csv)|*.csv|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName, true, Encoding.UTF8);
+                streamWriter.WriteLine(sav);
+                streamWriter.Close();
+            }
+           
+        }
+    }
+}
